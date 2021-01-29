8c4f06401a397dafc15dd2e94d406888787dca07
lordalfonso <alfonsoske@gmail.com>
2021-01-29 04:02:45 +0000

    se agrega fecha

--- RETO2.R
+++ RETO2.R
@@ -1,1 +1,17 @@
 #aqui va el reto 2
+
+library(rvest)
+
+theurl <- "https://www.glassdoor.com.mx/Sueldos/data-scientist-sueldo-SRCH_KO0,14.htm"
+
+file<-read_html(theurl)
+
+tables <- html_nodes(file, "table")  
+
+table1 <- html_table(tables[[1]], fill = TRUE)
+
+table <- na.omit(as.data.frame(table1))
+str(table)
+
+
+
